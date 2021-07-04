5a2403b245f3fc3e6cd0a804e7e27dd68c1f4873
Bodie Weedop <bweedop@pop-os.localdomain>
2021-07-04 11:45:56 -0600

    Modify setup.sh for new systems

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ mkdir ~/.vim/undodir/ -p
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+vim -c 'PlugInstall | quit' ~/.vim/vimrc
+
+sudo apt install build-essential cmake vim-nox python3-dev mono-complete golang nodejs default-jdk npm
+
+cd ~/.vim/plugged/YouCompleteMe
+
+python3 install.py --all
+
+cd ~/
