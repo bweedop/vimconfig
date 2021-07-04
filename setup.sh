651662d7b04087dcf32640c5778060bdf940262f
K. Bodie Weedop <kbweedop@gmail.com>
2021-07-04 12:04:36 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,18 @@
 
 # Initial setup after cloning repo
 
+# Make undodir directory
 mkdir ~/.vim/undodir/ -p
 
+# Download Plug vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# Install the plugins in the vimrc
 vim -c 'PlugInstall | quit' ~/.vim/vimrc
 
+# Install all software needed for YouCompleteMe
 sudo apt install build-essential cmake vim-nox python3-dev mono-complete golang nodejs default-jdk npm
-
+# Go to YCM dir and install
 cd ~/.vim/plugged/YouCompleteMe
-
 python3 install.py --all
-
+# Go back to home directory
 cd ~/
